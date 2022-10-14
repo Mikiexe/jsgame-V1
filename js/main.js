aba84909b3bc5d6366a2dab583665c2758effe31
Mikiexe <matias06112001@gmail.com>
2022-10-14 13:30:10 +0200

    Added functional menu, "New game" and "Settings" divs + a js function to move to wanted menu and to return to previous menu

--- js/main.js
+++ js/main.js
@@ -1,34 +1,40 @@
+// Pasan dos parametros, la id del que se enseñará, la id del que se esconderá
+// Se pasan en el documento html
+const goTo = (divId, hideDivId) => {
+  let showDiv = document.getElementById(divId);
+  let esconde = document.getElementById(hideDivId);
 
+  console.log(showDiv);
+  console.log(esconde);
+
+  showDiv.style.display = 'block';
+  esconde.style.display = 'none';
+};
 
 const hoverSlot = (backpack, slot) => {
+  // console.log("Tengo la id: " + backpack);
+  // console.log("Me pasan la id " + slot);
+  let sloti = document.getElementById(backpack);
 
-    // console.log("Tengo la id: " + backpack);
-    // console.log("Me pasan la id " + slot);
-    let sloti = document.getElementById(backpack);
-
-    sloti.removeAttribute("id", backpack);
-    sloti.setAttribute("id", slot);
-    // console.log("Quito la id:" + backpack);
-    // console.log("Pongo la id:" + slot);
-    // console.log("estas sobre mi");
-    // console.log("--------------------------");
-
-}
-
+  sloti.removeAttribute('id', backpack);
+  sloti.setAttribute('id', slot);
+  // console.log("Quito la id:" + backpack);
+  // console.log("Pongo la id:" + slot);
+  // console.log("estas sobre mi");
+  // console.log("--------------------------");
+};
 
 const unHoverSlot = (slot, backpack) => {
+  // console.log(slot);
+  // console.log(backpack);
 
-    // console.log(slot);
-    // console.log(backpack);
+  // console.log("Tengo la id slot: " + slot);
+  // console.log("Me pasan la id: " + backpack);
+  let sloti = document.getElementById(slot);
 
-    // console.log("Tengo la id slot: " + slot);
-    // console.log("Me pasan la id: " + backpack);
-    let sloti = document.getElementById(slot);
-
-    // console.log("Quito la id " + slot);
-    // console.log("Pongo de nuevo la id " + backpack);
-    sloti.removeAttribute("id", slot);
-    sloti.setAttribute("id", backpack);
-    // console.log("------------------");
-
-}
+  // console.log("Quito la id " + slot);
+  // console.log("Pongo de nuevo la id " + backpack);
+  sloti.removeAttribute('id', slot);
+  sloti.setAttribute('id', backpack);
+  // console.log("------------------");
+};
